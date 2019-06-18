44c4731f8f97aea5f768fa7ad0bd5baa948ac907
Keen Wilson <keencwilson@gmail.com>
2019-06-17 23:36:24 -0500

    Updates

---
+++ precache-manifest.983432187f5a6259a97e6d993ad95879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4c06b909c411ba3117a5bf15137f77",
+    "url": "/react-baramee-thai-bistro/index.html"
+  },
+  {
+    "revision": "87b73acb972297f6cf83",
+    "url": "/react-baramee-thai-bistro/static/css/2.38b937bc.chunk.css"
+  },
+  {
+    "revision": "05671b2866154054174f",
+    "url": "/react-baramee-thai-bistro/static/css/main.fc3a9803.chunk.css"
+  },
+  {
+    "revision": "87b73acb972297f6cf83",
+    "url": "/react-baramee-thai-bistro/static/js/2.c226289c.chunk.js"
+  },
+  {
+    "revision": "05671b2866154054174f",
+    "url": "/react-baramee-thai-bistro/static/js/main.b9612639.chunk.js"
+  },
+  {
+    "revision": "338f1e3ae9fd7aa18a7b",
+    "url": "/react-baramee-thai-bistro/static/js/runtime~main.8c712ea3.js"
+  }
+]);
