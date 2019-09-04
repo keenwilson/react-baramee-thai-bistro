58631f6abb6484de9cc567d28d942f8501557467
Keen <kwilson@finsignum.com>
2019-09-04 17:13:51 -0500

    Adding intro gallery for website

--- src/page/desktopHome.js
+++ src/page/desktopHome.js
@@ -23,6 +23,7 @@ import DinnerCurries from '../menu/dinner/curries'
 import DinnerDessertsDrinks from '../menu/dinner/dessertsDrinks'
 import DinnerHouseFavorites from '../menu/dinner/houseFavorites'
 import DesktopHeader from './desktopHeader'
+import Thumbnail from '../components/thumbnail'
 
 const { Content, Footer } = Layout
 
@@ -55,6 +56,57 @@ class DesktopHome extends Component {
   render() {
     return (
       <React.Fragment>
+        <div className="intro-gallery">
+          <Thumbnail
+            className="images"
+            imagePath="https://i.imgur.com/xLYTGIll.jpg"
+            imageAlt="Best Thai restaurant in Kansas City - Northeastern Thai"
+            isPortrait={false}
+          />
+          <Thumbnail
+            className="images"
+            imagePath="https://s3-media2.fl.yelpcdn.com/bphoto/FGHH7p9ZL40ogeMl0vPJbA/o.jpg"
+            imageAlt="Best Thai restaurant in Kansas City - Pad Ka Prao"
+            isPortrait={false}
+          />
+          <Thumbnail
+            className="images"
+            imagePath="https://s3-media3.fl.yelpcdn.com/bphoto/SoGFl9P4E-_urs0iRShUJw/o.jpg"
+            imageAlt="Seating Indoor"
+            isPortrait={false}
+          />
+          <Thumbnail
+            className="images"
+            imagePath="https://s3-media2.fl.yelpcdn.com/bphoto/nYrLq2A6VNlWHPWOglnqig/o.jpg"
+            imageAlt="Best Thai restaurant in Kansas City - Baramee Thai Platters"
+            isPortrait={false}
+          />
+          <Thumbnail
+            className="images"
+            imagePath="https://s3-media2.fl.yelpcdn.com/bphoto/Rw5tUfUDPfl0M4uouSZjBw/o.jpg"
+            imageAlt="Best Thai restaurant in Kansas City - Pad Thai"
+            isPortrait={true}
+          />
+          <Thumbnail
+            className="images"
+            imagePath="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/57331473_433008480799948_266372282560020480_n.jpg?_nc_cat=106&_nc_oc=AQlWzZ9pKl0K4FwyJ2maBgwKkUY7Tzxn8ewJSiJ4zlXZGqQgI1q_uGI6dmsZqZSeG9hN0lS9yUcgHFpe6RY8MPIt&_nc_ht=scontent-dfw5-1.xx&oh=a4814497c2d1b75b4921c454216406bd&oe=5DC4B712"
+            imageAlt="Seating Outdoor: Red Umbrella"
+            isPortrait={true}
+          />
+          <Thumbnail
+            className="images"
+            imagePath="https://s3-media1.fl.yelpcdn.com/bphoto/YiX9_BAtBmSUV9iFYfZRyw/o.jpg"
+            imageAlt="Best Thai restaurant in Kansas City - Red Curries"
+            isPortrait={false}
+          />
+
+          <Thumbnail
+            className="images"
+            imagePath="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/51366392_393494678084662_2627909008874274816_n.jpg?_nc_cat=106&_nc_oc=AQnvhOo89vT7Qod75Z-DRznSZiZzwiGWXohYf64VpHECvCz4vEphU_6H1qUlduyCYCKTz2-uLdwh-Z9ZRPZzwSWd&_nc_ht=scontent-dfw5-1.xx&oh=cba696737daaef6a2936514619ae2cbd&oe=5D8497F2"
+            imageAlt="Best Thai restaurant in Kansas City - Papaya Salad"
+            isPortrait={true}
+          />
+        </div>
         <div className="desktop-narrative">
           <h1>Baramee Thai Bistro</h1>
           <p>
