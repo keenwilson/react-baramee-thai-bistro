1b36f9d328d7b8946460a66c304b3a8bb8e46f27
Keen <kwilson@finsignum.com>
2019-10-04 17:35:44 -0500

    Set up flex box for menu

--- src/page/desktopIndex.js
+++ src/page/desktopIndex.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
-import { Icon } from 'antd'
+import { Route } from 'react-router-dom'
 import DesktopHeader from './desktopHeader'
 import DesktopFooter from './desktopFooter'
 import DesktopHome from './desktopHome'
 import DesktopLunchIndex from './desktopLunchIndex'
 import DesktopDinnerIndex from './desktopDinnerIndex'
+import DesktopMenuIndex from './desktopMenuIndex'
 
 const DesktopIndex = () => {
   return (
@@ -13,6 +13,7 @@ const DesktopIndex = () => {
       <DesktopHeader />
       <div className="desktop-content-display">
         <Route exact path={'/'} component={DesktopHome} />
+        <Route path={'/menu'} component={DesktopMenuIndex} />
         <Route path={'/menu/lunch'} component={DesktopLunchIndex} />
         <Route path={'/menu/dinner'} component={DesktopDinnerIndex} />
       </div>
---
+++ src/page/desktopMenuIndex.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+
+const DesktopMenuIndex = () => {
+  return (
+    <React.Fragment>
+      <div className="heading-title underline-60">Our Menu</div>
+      <div className="flex-box">
+        <div className="item-1">Lunch</div>
+        <div className="item-1">Dinner</div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default DesktopMenuIndex
