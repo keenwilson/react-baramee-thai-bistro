afe833144d99087f9ca371f0f00b3c889c83bd68
Keen <kwilson@finsignum.com>
2019-10-04 17:19:07 -0500

    Setting up side navigation

--- src/page/desktopLunchIndex.js
+++ src/page/desktopLunchIndex.js
@@ -11,23 +11,37 @@ import LunchCurries from '../menu/lunch/curries'
 import LunchDesserts from '../menu/lunch/desserts'
 import LunchDrinks from '../menu/lunch/drinks'
 import DesktopLunchHome from './desktopLunchHome'
+import { Layout } from 'antd'
+import DesktopLunchMenuNav from './desktopLunchMenuNav'
+
+const { Sider, Content } = Layout
 
 const DesktopLunchIndex = () => {
   return (
     <React.Fragment>
-      <div className="narrative">
-        <Route path="/menu/lunch" component={DesktopLunchHome} />
-        <Route path="/menu/lunch/appetizers" component={LunchAppetizers} />
-        <Route path="/menu/lunch/salads" component={LunchSalads} />
-        <Route path="/menu/lunch/sides" component={LunchSides} />
-        <Route path="/menu/lunch/from-the-wok" component={LunchFromTheWok} />
-        <Route path="/menu/lunch/fried-rice" component={LunchFriedRice} />
-        <Route path="/menu/lunch/noodles" component={LunchNoodles} />
-        <Route path="/menu/lunch/soups" component={LunchSoups} />
-        <Route path="/menu/lunch/curries" component={LunchCurries} />
-        <Route path="/menu/lunch/desserts" component={LunchDesserts} />
-        <Route path="/menu/lunch/drinks" component={LunchDrinks} />
-      </div>
+      <Layout className="top-padding">
+        <Sider>
+          <DesktopLunchMenuNav />
+        </Sider>
+        <Content>
+          <div className="menu-lunch-navigatio">
+            <Route path="/menu/lunch" component={DesktopLunchHome} />
+            <Route path="/menu/lunch/appetizers" component={LunchAppetizers} />
+            <Route path="/menu/lunch/salads" component={LunchSalads} />
+            <Route path="/menu/lunch/sides" component={LunchSides} />
+            <Route
+              path="/menu/lunch/from-the-wok"
+              component={LunchFromTheWok}
+            />
+            <Route path="/menu/lunch/fried-rice" component={LunchFriedRice} />
+            <Route path="/menu/lunch/noodles" component={LunchNoodles} />
+            <Route path="/menu/lunch/soups" component={LunchSoups} />
+            <Route path="/menu/lunch/curries" component={LunchCurries} />
+            <Route path="/menu/lunch/desserts" component={LunchDesserts} />
+            <Route path="/menu/lunch/drinks" component={LunchDrinks} />
+          </div>
+        </Content>
+      </Layout>
     </React.Fragment>
   )
 }
---
+++ src/page/desktopLunchMenuNav.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Menu } from 'antd'
+
+const DesktopLunchMenuNav = () => {
+  return (
+    <Menu
+      style={{ width: 256 }}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['1']}
+      mode="inline"
+    >
+      <Menu.Item key="1">
+        <Link to="/menu/lunch/appetizers" className="menu-meal">
+          Appetizers
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <Link to="/menu/lunch/salads" className="menu-meal">
+          Salads
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <Link to="/menu/lunch/fried-rice" className="menu-meal">
+          Fried Rice
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="4">
+        <Link to="/menu/lunch/from-the-wok" className="menu-meal">
+          From the Wok
+        </Link>
+      </Menu.Item>
+
+      <Menu.Item key="5">
+        <Link to="/menu/lunch/noodles" className="menu-meal">
+          Noodles
+        </Link>
+      </Menu.Item>
+
+      <Menu.Item key="6">
+        <Link to="/menu/lunch/soups" className="menu-meal">
+          Soups
+        </Link>
+      </Menu.Item>
+
+      <Menu.Item key="7">
+        <Link to="/menu/lunch/curries" className="menu-meal">
+          Curries
+        </Link>
+      </Menu.Item>
+
+      <Menu.Item key="8">
+        <Link to="/menu/lunch/sides" className="menu-meal">
+          Sides
+        </Link>
+      </Menu.Item>
+
+      <Menu.Item key="9">
+        <Link to="/menu/lunch/desserts" className="menu-meal">
+          Desserts
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="10">
+        <Link to="/menu/lunch/drinks" className="menu-meal">
+          Drinks
+        </Link>
+      </Menu.Item>
+    </Menu>
+  )
+}
+
+export default DesktopLunchMenuNav
