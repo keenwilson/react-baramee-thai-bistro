6ecc12bb9099f4f5457b9ca019b188b625354af6
Keen <keencwilson@gmail.com>
2019-11-07 10:57:34 -0600

    Updates

---
+++ precache-manifest.6c4863e93e4f79b712616e4ddcd28695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1744484abc385b2bd573bfdcb16f0b00",
+    "url": "/react-baramee-thai-bistro/index.html"
+  },
+  {
+    "revision": "99a557526eb60472f40c",
+    "url": "/react-baramee-thai-bistro/static/css/2.7bb9430f.chunk.css"
+  },
+  {
+    "revision": "c1c525bc1914ff2550b8",
+    "url": "/react-baramee-thai-bistro/static/css/main.63f61694.chunk.css"
+  },
+  {
+    "revision": "99a557526eb60472f40c",
+    "url": "/react-baramee-thai-bistro/static/js/2.6aecd172.chunk.js"
+  },
+  {
+    "revision": "c1c525bc1914ff2550b8",
+    "url": "/react-baramee-thai-bistro/static/js/main.c9e62974.chunk.js"
+  },
+  {
+    "revision": "338f1e3ae9fd7aa18a7b",
+    "url": "/react-baramee-thai-bistro/static/js/runtime~main.8c712ea3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-baramee-thai-bistro/precache-manifest.35aec802af27876237e05e84377b1f11.js"
+  "/react-baramee-thai-bistro/precache-manifest.6c4863e93e4f79b712616e4ddcd28695.js"
 );
 
 self.addEventListener('message', (event) => {
