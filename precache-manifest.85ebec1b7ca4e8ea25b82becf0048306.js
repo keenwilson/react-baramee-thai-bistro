d649f1ec12dcba67eecf5466fc35125f2a474c6e
Keen <keencwilson@gmail.com>
2020-01-06 18:28:45 -0600

    Updates

---
+++ precache-manifest.85ebec1b7ca4e8ea25b82becf0048306.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b77b64617f3663164c13b863b06a784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf082fbc7fe8cf4c1c7f",
+    "url": "/static/css/2.7bb9430f.chunk.css"
+  },
+  {
+    "revision": "3d45d8efb164a1c280b1",
+    "url": "/static/css/main.dc4ff1b8.chunk.css"
+  },
+  {
+    "revision": "bf082fbc7fe8cf4c1c7f",
+    "url": "/static/js/2.069ccf5f.chunk.js"
+  },
+  {
+    "revision": "3d45d8efb164a1c280b1",
+    "url": "/static/js/main.f5080392.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-baramee-thai-bistro/precache-manifest.672a0b05bce5f514ae119834008115ae.js"
+  "/precache-manifest.85ebec1b7ca4e8ea25b82becf0048306.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-baramee-thai-bistro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
